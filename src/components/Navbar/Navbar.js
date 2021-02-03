9e5cf27b782cae5f682d5c6a5ad1349e2b023145
Katie Fletcher <77756012+kfletch2024@users.noreply.github.com>
2021-02-02 21:54:07 -0500

    navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'; 
+import { MenueItems } from "./MenueItems"
+import { Button } from "../Button"
+import './Navbar.css'
+
+class Navbar extends Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
+    render() {
+        return(
+            <nav className="NavbarItems"> 
+            <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
+            <div className="menue-icon" onClick={this.handleClick}>
+                <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+            </div>
+            <ul className={this.state.clicked ? 'nav-menue active' : 'nav-menue'}> 
+                {MenueItems.map((item, index)=> {
+                    return (
+                        <li key={index}> 
+                            <a className={item.cName} href={item.url}>
+                            {item.title}
+                            </a>
+                        </li>
+                    )
+
+                })}
+                
+            </ul>
+            <Button>Sign Up</Button>
+            </nav>
+        )
+    }
+}
+export default Navbar
